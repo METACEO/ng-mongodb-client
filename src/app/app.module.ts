98ed358f3b96e7857e169f903dec53f0c02436a9
James Allen <james.allen@meta.ceo>
2018-07-17 19:55:20 -0500

    npm run ng -- generate @ngrx/schematics:effect App --root --module app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AppEffects } from './app.effects';
 import { reducers, metaReducers } from './reducers';
 
 @NgModule({
@@ -23,7 +25,9 @@ import { reducers, metaReducers } from './reducers';
     MatPaginatorModule,
     MatTableModule,
     MatToolbarModule,
-    StoreModule.forRoot(reducers, { metaReducers }), !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent]
